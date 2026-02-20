b0edc190b57ef1a1ccbe57095e395f6d67bc9d63
Lumix1337 <olzagula@gmail.com>
2026-02-20 20:44:58 +0500

    Task #1

---
+++ Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Task
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter title of movie:");
+        string MovieTitle = Console.ReadLine();
+
+        Console.WriteLine("Number of tickets:");
+        int TicketNumbers = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Price per ticket:");
+        int PriceOfTickets = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Duration of movie in hours:");
+        int Duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Screen number");
+        int ScreenNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("First letter of Seat row:");
+        string SeatLetter = Console.ReadLine();
+
+        Console.WriteLine("Is 3D movie (true/false):");
+        Boolean ThreeD = Convert.ToBoolean(Console.ReadLine());
+
+        int CostPerHour = PriceOfTickets / Duration;
+
+        Console.WriteLine($"Movies title is {MovieTitle},\nTotal cost of your tickets: {TicketNumbers * PriceOfTickets},\nSeat number {SeatLetter},\nCost per hour : {CostPerHour}");
+    }
+}
